6ee1ae555b6762e00c90a7e9ab170c80493a178b
lolrafael123 <rafael.antunes2705@gmail.com>
2024-10-19 23:30:38 -0300

    form

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,3 +11,40 @@ const last = document.querySelector("#lastModified");
 const lastDate = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
 last.innerHTML = `Last Modification: <span>${oLastModif.toLocaleString('en-US', lastDate)}</span>`;
 
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+  const selectElement = document.getElementById('product');
+
+
+products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id; 
+    option.textContent = `${product.name} - Average Rating: ${product.averagerating}`;
+    selectElement.appendChild(option);
+});
